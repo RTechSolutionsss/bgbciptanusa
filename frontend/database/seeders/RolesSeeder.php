8502d621e87d6636d2b8860581da86e3986bf758
KurniaMulyaR <63498139+KurniaMulyaR@users.noreply.github.com>
2023-02-27 22:55:19 +0700

    db seed for admin

--- frontend/database/seeders/RolesSeeder.php
+++ frontend/database/seeders/RolesSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\UserRole;
+use Illuminate\Support\Facades\Hash;
 
 class RolesSeeder extends Seeder
 {
@@ -15,17 +16,24 @@ class RolesSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\UserRole::create([
-            'role_name' => 'ADMIN',
-        ]);
-        \App\Models\UserRole::create([
-            'role_name' => 'SALES',
-        ]);
-        \App\Models\UserRole::create([
-            'role_name' => 'CUSTOMER',
-        ]);
-        \App\Models\UserRole::create([
-            'role_name' => 'MARKOM',
+        // \App\Models\UserRole::create([
+        //     'role_name' => 'ADMIN',
+        // ]);
+        // \App\Models\UserRole::create([
+        //     'role_name' => 'SALES',
+        // ]);
+        // \App\Models\UserRole::create([
+        //     'role_name' => 'CUSTOMER',
+        // ]);
+        // \App\Models\UserRole::create([
+        //     'role_name' => 'MARKOM',
+        // ]);
+
+        \App\Models\User::create([
+            'name' => 'nanda',
+            'email' => 'nanda.berliana@citanusa.com',
+            'password' => Hash::make('123123123'),
+            'role_id' => '1'
         ]);
     }
 }
