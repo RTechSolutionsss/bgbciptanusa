3ab3534d63a5896aadb4601accba413f66c33837
KurniaMulyaR <63498139+KurniaMulyaR@users.noreply.github.com>
2023-04-08 14:26:03 +0700

    bugs wa

--- frontend/app/Http/Controllers/UserSalesController.php
+++ frontend/app/Http/Controllers/UserSalesController.php
@@ -99,7 +99,7 @@ class UserSalesController extends Controller
     {
         $base_url =  $_ENV['APP_URL'];
         $user = User::with('usersales')->findOrFail($id);
-        $link =  $base_url + route('customer.show', Crypt::encrypt($user->usersales->link));
-        return Redirect::to('https://wa.me/+62'.$user->phone.'?text=Selamat%20Bergabung%20dalam%20Program%20BGB%A0Citanusa%20Group!%20%20%20%20%20Berikut%20link%20akun%20anda:'. $link .'%20%20%20%20%20%20%20ID:%20%20:'. $user->email .'%20%20%20%20%20password%20:%20'. $user->first_password . '%20%20%20%20%20%20%20%20%20Happy%20Selling!url='. $base_url);
+        $link =  route('customer.show', Crypt::encrypt($user->usersales->link));
+        return Redirect::to('https://wa.me/+62'.$user->phone.'?text=Selamat%20Bergabung%20dalam%20Program%20BGB%A0Citanusa%20Group!%20%20%20%20%20Berikut%20link%20akun%20anda:'. $link .'%20%20%20%20%20%20%20ID:%20%20:'. $user->email .'%20%20%20%20%20password%20:%20'. $user->first_password . '%20%20%20%20%20%20%20%20%20Happy%20Selling!%20url=%20'. $base_url);
     }
 }
